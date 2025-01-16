9ccdff3ca2ff65c9ed175aace268e9b02f81b754
Ibrahim <ibra.rodriguez.olaya@gmail.com>
2025-01-16 10:44:40 -0600

    Estructura y configuración del proyecto

---
+++ applications/reporte/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Reporte(models.Model):
+    dia = models.DateField(verbose_name="Día")
+    hora = models.TimeField(verbose_name="Hora")
+    tema = models.CharField(verbose_name="Tema", max_length=200)
+    integrantes = models.TextField(verbose_name="Integrantes")
+    lugar = models.CharField(verbose_name="Lugar", max_length=200)
+
+    class Meta:
+        verbose_name = "Reporte"
+        verbose_name_plural = "Reportes"
+        db_table = "reporte"
+
+    def __str__(self):
+        return f"{self.tema} - {self.dia}:{self.hora}"
